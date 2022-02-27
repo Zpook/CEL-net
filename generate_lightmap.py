5f3ae8d01fb3afb4d50d5e99c7f3b23202d35ad4
Mikel <mikelk96@gmail.com>
2022-02-27 17:18:56 +0200

    relight "training" code

--- generate_lightmap.py
+++ generate_lightmap.py
@@ -2,13 +2,16 @@ import imp
 from typing import Dict
 import functools
 
-from rawrelight import LightMap
-from image_dataset.dataset_loaders.CEL import CELImage, CELDatasetLoader, cel_filters
+from lightmap import LightMap
+from image_dataset.dataset_loaders.CEL import CELImage, RawCELDatasetLoader, cel_filters
 
 INPUT_EXPOSURES = [0.1]
 OUTPUT_EXPOSURES = [10]
 
 TRAIN_JSON: str = "./dataset/train.JSON"
+
+
+MAX_RELIGHT_LEVEL = 5000
 
 
 FILTER_SCENARIOS =  [2001,2002,2003,2004,2005]
@@ -23,9 +26,18 @@ def Run():
     inputFilters = functools.partial(cel_filters.Chain,[inputExposuresFilter,filterScenarios])
     truthFilters = functools.partial(cel_filters.Chain,[truthExposuresFilter,filterScenarios])
 
-    datasetLoader = CELDatasetLoader(TRAIN_JSON,inputFilters,truthFilters)
+    datasetLoader = RawCELDatasetLoader(TRAIN_JSON,inputFilters,truthFilters)
     sets = datasetLoader.GetSet()
-    pass
+
+    lightmap = LightMap(MAX_RELIGHT_LEVEL,4,-1)
+
+    for set in sets:
+        input, truth = set.GetPair()
+        lightmap.SampleImage(input,truth)
+
+    lightmap.Save("./local/lightmap.bin")
+
+
 
 
 
